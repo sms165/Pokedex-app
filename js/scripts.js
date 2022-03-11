2ba19d132f3720c33cff94468135856df452444c
sms165 <61697688+sms165@users.noreply.github.com>
2022-03-11 09:35:14 +0100

    second exercise: created array filled with objects of pokemon characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
-alert('Hello World');
+let pokemonList = [];
 
-let favoriteFood = "Pasta";
-document.write(favoriteFood);
+let  bulbasaur = {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison'],
+    abilities: ['chlorophyll', 'overgrow']
+};
+
+let  charmander = {
+    name: 'Charmander',
+    height: 6,
+    types: ['fire'],
+    abilities: ['blaze', 'solar-power']
+};
+
+let  squirtle = {
+    name: 'Squirtle',
+    height: 5,
+    types: ['water'],
+    abilities: ['rain-dish', 'torrent']
+};
+
+let  pikachu = {
+    name: 'Pikachu',
+    height: 4,
+    types: ['electric'],
+    abilities: ['static', 'lightningrod']
+};
+
+pokemonList= [bulbasaur, charmander, squirtle, pikachu];
+
+console.log(pokemonList[0]);
+console.log(pokemonList.includes(charmander));
