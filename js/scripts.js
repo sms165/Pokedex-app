e682ed4d957a592e472a645e381a48a130971bc4
sms165 <61697688+sms165@users.noreply.github.com>
2022-03-18 13:03:33 +0100

    exercise 1.5 Functions Part 2: added IIFE to make the app more stable

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,89 @@
-let pokemonList = [];
+//IIFE
+let pokemonRepository= (function () {
+    let pokemonList = [];
 
-let  bulbasaur = {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', ' poison'],
-    abilities: ['chlorophyll', ' overgrow']
-};
-
-let  charmander = {
-    name: 'Charmander',
-    height: 6,
-    types: ['fire'],
-    abilities: ['blaze', ' solar-power']
-};
-
-let  squirtle = {
-    name: 'Squirtle',
-    height: 5,
-    types: ['water'],
-    abilities: ['rain-dish', ' torrent']
-};
-
-let  pikachu = {
-    name: 'Pikachu',
-    height: 4,
-    types: ['electric'],
-    abilities: ['static', ' lightningrod']
-};
-
-pokemonList= [bulbasaur, charmander, squirtle, pikachu];
-
-console.log(pokemonList[0]);
-console.log(pokemonList.includes(charmander));
-
-let pl =[
-    {
+    //pokemon objects
+    let  bulbasaur = {
         name: 'Bulbasaur',
         height: 7,
-        types: ['grass', 'poison'],
-        abilities: ['chlorophyll', 'overgrow']
-    },
-    {
+        types: ['grass', ' poison'],
+        abilities: ['chlorophyll', ' overgrow']
+    };
+    
+    let  charmander = {
         name: 'Charmander',
         height: 6,
         types: ['fire'],
-        abilities: ['blaze', 'solar-power']
-    },
-    {
+        abilities: ['blaze', ' solar-power']
+    };
+    
+    let  squirtle = {
         name: 'Squirtle',
         height: 5,
         types: ['water'],
-        abilities: ['rain-dish', 'torrent']
-    },
-    {
+        abilities: ['rain-dish', ' torrent']
+    };
+    
+    let  pikachu = {
         name: 'Pikachu',
         height: 4,
         types: ['electric'],
-        abilities: ['static', 'lightningrod']
+        abilities: ['static', ' lightningrod']
+    };
+    
+    pokemonList= [bulbasaur, charmander, squirtle, pikachu];
+
+    //functions
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+        
     }
-];
+    
+    function getAll () {
+        return pokemonList
+        
+    }
+    
+    return{
+        add: add,
+        getAll:getAll
+    };
+
+  
+})();
+
+
+// let pl =[
+//     {
+//         name: 'Bulbasaur',
+//         height: 7,
+//         types: ['grass', 'poison'],
+//         abilities: ['chlorophyll', 'overgrow']
+//     },
+//     {
+//         name: 'Charmander',
+//         height: 6,
+//         types: ['fire'],
+//         abilities: ['blaze', 'solar-power']
+//     },
+//     {
+//         name: 'Squirtle',
+//         height: 5,
+//         types: ['water'],
+//         abilities: ['rain-dish', 'torrent']
+//     },
+//     {
+//         name: 'Pikachu',
+//         height: 4,
+//         types: ['electric'],
+//         abilities: ['static', 'lightningrod']
+//     }
+// ];
 // console.log(pl[0]);
 
 
-// for (let i = 0; i < pl.length; i++) {
-    
-//     console.log(pl[i].name);
-    
-// }
-// document.write('<div class=\'card-main\'>')
-// for (let i = 0; i < pokemonList.length; i++) {
-//     document.write('<div class=\'card\'><h2>' +pokemonList[i].name + '</h2><br>');
-//     document.write('Height: '+pokemonList[i].height);
-//     if (pokemonList[i].height>6) {
-//         document.write('<span class=\'size\'> -Wow, that\'s big </span><br>');
-//     }else{
-//         document.write('<br>');
-//     }
-//     document.write('Types: '+ pokemonList[i].types + '<br>');
-//     document.write('Abilities: ' + pokemonList[i].abilities+ '<br> </div>');
-// }
-// document.write('</div>')
+//use of IIFE
+let pokemonList =pokemonRepository.getAll();
 
 document.write('<div class=\'card-main\'>')
 pokemonList.forEach(function(pokemon) {
@@ -92,8 +96,6 @@ pokemonList.forEach(function(pokemon) {
     }
     document.write('Types: '+ pokemon.types + '<br>');
     document.write('Abilities: ' + pokemon.abilities+ '<br> </div>');
-
-
 });
 
 document.write('</div>')
