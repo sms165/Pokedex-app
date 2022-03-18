3e1769fc9166f43618830697207b3ee677a7d7c2
sms165 <61697688+sms165@users.noreply.github.com>
2022-03-18 12:03:21 +0100

    exercise 1.5 Functions Part 2: changed for loop to foreach function

--- js/scripts.js
+++ js/scripts.js
@@ -33,34 +33,32 @@ pokemonList= [bulbasaur, charmander, squirtle, pikachu];
 console.log(pokemonList[0]);
 console.log(pokemonList.includes(charmander));
 
-// let pl =[
-//     {
-//         name: 'Bulbasaur',
-//         height: 7,
-//         types: ['grass', 'poison'],
-//         abilities: ['chlorophyll', 'overgrow']
-//     },
-//     {
-//         name: 'Charmander',
-//         height: 6,
-//         types: ['fire'],
-//         abilities: ['blaze', 'solar-power']
-//     },
-//     {
-//         name: 'Squirtle',
-//         height: 5,
-//         types: ['water'],
-//         abilities: ['rain-dish', 'torrent']
-//     },
-//     {
-//         name: 'Pikachu',
-//         height: 4,
-//         types: ['electric'],
-//         abilities: ['static', 'lightningrod']
-//     }
-
-
-// ];
+let pl =[
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison'],
+        abilities: ['chlorophyll', 'overgrow']
+    },
+    {
+        name: 'Charmander',
+        height: 6,
+        types: ['fire'],
+        abilities: ['blaze', 'solar-power']
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        types: ['water'],
+        abilities: ['rain-dish', 'torrent']
+    },
+    {
+        name: 'Pikachu',
+        height: 4,
+        types: ['electric'],
+        abilities: ['static', 'lightningrod']
+    }
+];
 // console.log(pl[0]);
 
 
@@ -69,18 +67,35 @@ console.log(pokemonList.includes(charmander));
 //     console.log(pl[i].name);
     
 // }
+// document.write('<div class=\'card-main\'>')
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write('<div class=\'card\'><h2>' +pokemonList[i].name + '</h2><br>');
+//     document.write('Height: '+pokemonList[i].height);
+//     if (pokemonList[i].height>6) {
+//         document.write('<span class=\'size\'> -Wow, that\'s big </span><br>');
+//     }else{
+//         document.write('<br>');
+//     }
+//     document.write('Types: '+ pokemonList[i].types + '<br>');
+//     document.write('Abilities: ' + pokemonList[i].abilities+ '<br> </div>');
+// }
+// document.write('</div>')
+
 document.write('<div class=\'card-main\'>')
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<div class=\'card\'><h2>' +pokemonList[i].name + '</h2><br>');
-    document.write('Height: '+pokemonList[i].height);
-    if (pokemonList[i].height>6) {
+pokemonList.forEach(function(pokemon) {
+    document.write('<div class=\'card\'><h2>' +pokemon.name + '</h2><br>');
+    document.write('Height: '+pokemon.height);
+    if (pokemon.height>6) {
         document.write('<span class=\'size\'> -Wow, that\'s big </span><br>');
     }else{
         document.write('<br>');
     }
-    document.write('Types: '+ pokemonList[i].types + '<br>');
-    document.write('Abilities: ' + pokemonList[i].abilities+ '<br> </div>');
-}
+    document.write('Types: '+ pokemon.types + '<br>');
+    document.write('Abilities: ' + pokemon.abilities+ '<br> </div>');
+
+
+});
+
 document.write('</div>')
 
-    
+
