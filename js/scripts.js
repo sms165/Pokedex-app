d42037713945061af8b5d2a059f3a6b1eb8411a4
sms165 <61697688+sms165@users.noreply.github.com>
2022-03-30 22:36:13 +0200

    footer with back to top button
    Implementation of font awesome cdn

--- js/scripts.js
+++ js/scripts.js
@@ -290,9 +290,6 @@ pokemonRepository.loadList().then(function () {
 })
 
 
-//find pokemon by name
-pokemonRepository.find('Bulbasaur');
-
 //use of IIFE
 let pokemonList = pokemonRepository.getAll();
 
@@ -301,3 +298,28 @@ let pokemonList = pokemonRepository.getAll();
 pokemonList.forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
